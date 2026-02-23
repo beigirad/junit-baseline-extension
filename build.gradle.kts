4e29ecc4acaae9d1bd585977e6d21c4fa0be5a5b
Farhad Beigirad <f.beigirad@gmail.com>
2026-02-23 14:50:40 +0330

    fix: remove unused kotest dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,8 @@ repositories {
 
 dependencies {
     implementation("com.squareup.moshi:moshi:1.15.2")
-    implementation("io.kotest:kotest-runner-junit5:5.9.1")
+    implementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
     testImplementation(kotlin("test"))
     testImplementation("org.junit.platform:junit-platform-testkit:1.10.1")
 }
